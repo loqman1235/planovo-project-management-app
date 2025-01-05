a3ce67c8f5f948e08049634cae50da2c1c48b13b
loqman1235 <tonyvito12@gmail.com>
2025-01-05 12:22:48 +0100

    refactor KanbanBoardContext: simplify drag-and-drop handlers and update dependencies

--- app/workspaces/[id]/tasks/_context/KanbanBoardContext.tsx
+++ app/workspaces/[id]/tasks/_context/KanbanBoardContext.tsx
@@ -1,35 +1,26 @@
 "use client";
 
 import { INITIAL_COLUMNS } from "@/data";
-import { Column, Task } from "@/types";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import { Column } from "@/types";
+import React, { createContext, useContext, useState } from "react";
+import { DragEndEvent, DragOverEvent, DragStartEvent } from "@dnd-kit/core";
 
 type KanbanBoardContext = {
   columns: Column[];
   setColumns: React.Dispatch<React.SetStateAction<Column[]>>;
   createTask: (title: string, collId: string) => void;
-  handleDragOver: (e: React.DragEvent, collId: string, tasks: Task[]) => void;
-  handleDragStart: (taskId: string) => void;
-  handleDragEnd: () => void;
-  handleDrag: (taskId: string) => void;
-  handleDrop: (e: React.DragEvent, collId: string) => void;
-  draggingTaskId: string | null;
-  dropTargetColumn: string | null;
-  dropIndex: number | null;
+  handleDragStart: (e: DragStartEvent) => void;
+  handleDragEnd: (e: DragEndEvent) => void;
+  handleDragOver: (e: DragOverEvent) => void;
 };
 
 export const KanbanBoardContext = createContext<KanbanBoardContext>({
   columns: INITIAL_COLUMNS,
   setColumns: () => {},
   createTask: () => {},
-  handleDragOver: () => {},
   handleDragStart: () => {},
   handleDragEnd: () => {},
-  handleDrag: () => {},
-  handleDrop: () => {},
-  draggingTaskId: null,
-  dropTargetColumn: null,
-  dropIndex: null,
+  handleDragOver: () => {},
 });
 
 export const KanbanBoardProvider = ({
@@ -38,9 +29,6 @@ export const KanbanBoardProvider = ({
   children: React.ReactNode;
 }) => {
   const [columns, setColumns] = useState<Column[]>(INITIAL_COLUMNS);
-  const [draggingTaskId, setDraggingTaskId] = useState<string | null>(null);
-  const [dropIndex, setDropIndex] = useState<number | null>(null);
-  const [dropTargetColumn, setDropTargetColumn] = useState<string | null>(null);
 
   console.log("columns", columns);
 
@@ -62,91 +50,9 @@ export const KanbanBoardProvider = ({
     });
   };
 
-  const handleDragOver = (
-    e: React.DragEvent,
-    collId: string,
-    tasks: Task[]
-  ) => {
-    e.preventDefault();
-    setDropTargetColumn(collId);
-
-    let newDropIndex = tasks.length;
-
-    for (let i = 0; i < tasks.length; i++) {
-      const taskElement = document.getElementById(tasks[i].id);
-      if (taskElement) {
-        const taskElementBounding = taskElement.getBoundingClientRect();
-        const taskElementMiddle =
-          taskElementBounding.top + taskElementBounding.height / 2;
-
-        if (e.clientY < taskElementMiddle) {
-          newDropIndex = i;
-          break;
-        }
-      }
-    }
-
-    setDropIndex(newDropIndex);
-  };
-
-  const handleDragStart = (taskId: string) => {
-    setDraggingTaskId(taskId);
-  };
-
-  const handleDrag = (taskId: string) => {
-    setDraggingTaskId(taskId);
-  };
-
-  const handleDragEnd = () => {
-    setDraggingTaskId(null);
-    setDropIndex(null);
-    setDropTargetColumn(null);
-  };
-
-  const handleDrop = (e: React.DragEvent, collId: string) => {
-    e.preventDefault();
-
-    setColumns((prevColumns) => {
-      const updatedColumns = [...prevColumns];
-
-      const sourceColumnIndex = updatedColumns.findIndex((col) =>
-        col.tasks.some((t) => t.id === draggingTaskId)
-      );
-
-      const targetColumnIndex = updatedColumns.findIndex(
-        (col) => col.id === collId
-      );
-
-      if (sourceColumnIndex === -1 || targetColumnIndex === -1)
-        return prevColumns;
-
-      const sourceTasks = [...updatedColumns[sourceColumnIndex].tasks];
-      const targetTasks = [...updatedColumns[targetColumnIndex].tasks];
-
-      const taskIndex = sourceTasks.findIndex((t) => t.id === draggingTaskId);
-      if (taskIndex === -1) return prevColumns;
-      const [task] = sourceTasks.splice(taskIndex, 1);
-
-      if (sourceColumnIndex === targetColumnIndex) {
-        sourceTasks.splice(dropIndex ?? sourceTasks.length, 0, task);
-        updatedColumns[sourceColumnIndex].tasks = sourceTasks;
-      } else {
-        targetTasks.splice(dropIndex ?? sourceTasks.length, 0, task);
-        updatedColumns[sourceColumnIndex].tasks = sourceTasks;
-        updatedColumns[targetColumnIndex].tasks = targetTasks;
-      }
-
-      return updatedColumns;
-    });
-
-    setDraggingTaskId(null);
-    setDropIndex(null);
-    setDropTargetColumn(null);
-  };
-
-  useEffect(() => {
-    console.log(columns);
-  }, [columns]);
+  const handleDragStart = (e: DragStartEvent) => {};
+  const handleDragEnd = (e: DragEndEvent) => {};
+  const handleDragOver = (e: DragOverEvent) => {};
 
   return (
     <KanbanBoardContext.Provider
@@ -154,14 +60,9 @@ export const KanbanBoardProvider = ({
         columns,
         setColumns,
         createTask,
-        handleDragOver,
         handleDragStart,
         handleDragEnd,
-        handleDrag,
-        handleDrop,
-        draggingTaskId,
-        dropTargetColumn,
-        dropIndex,
+        handleDragOver,
       }}
     >
       {children}
