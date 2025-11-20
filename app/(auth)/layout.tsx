e98af41b3f7aed2869d33fdfeccadbd555aa423a
loqman1235 <tonyvito12@gmail.com>
2025-11-20 08:56:57 +0100

    fix: Fix auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -14,7 +14,7 @@ const AutLayout = async ({ children }: AutLayoutProps) => {
     <div className="w-full min-h-screen flex flex-col gap-5 items-center justify-center p-5">
       <Brand />
 
-      <div className="relative min-w-[454px]">
+      <div className="relative w-full max-w-[454px]">
         <div className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 bg-primary w-[138px] h-[138px] rounded-full blur-[180px] -z-10"></div>
 
         <div className="w-full bg-foreground rounded-2xl p-10 border-t border-t-border-light flex flex-col gap-10">
