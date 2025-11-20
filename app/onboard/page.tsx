85f06fbae7a4dc182e7d6e91acd5f09eb94e1847
loqman1235 <tonyvito12@gmail.com>
2025-11-20 08:41:07 +0100

    fix: Fix onboard page

--- app/onboard/page.tsx
+++ app/onboard/page.tsx
@@ -6,8 +6,23 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import CreateInitialWorkspaceForm from "./_components/CreateInitialWorkspaceForm";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { getDefaultWorkspace } from "@/lib/workspace";
 
-const OnboardPage = () => {
+const OnboardPage = async () => {
+  const session = await auth();
+
+  if (!session?.user) {
+    redirect("/sign-in");
+  }
+
+  const defaultWorkspace = await getDefaultWorkspace(session.user.id);
+
+  if (defaultWorkspace) {
+    redirect(`/workspaces/${defaultWorkspace.id}`);
+  }
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <Card>
