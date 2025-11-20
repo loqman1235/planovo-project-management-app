d5d4116bcfab4f0ef5a996d43c55b26ca1aa4d02
loqman1235 <tonyvito12@gmail.com>
2025-11-20 11:01:52 +0100

    update: remove GithubSignIn button

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,5 @@
 import { Brand } from "@/components/Brand";
-import { GithubSignInBtn } from "./_components/GithubSignInBtn";
+// import { GithubSignInBtn } from "./_components/GithubSignInBtn";
 
 type AutLayoutProps = {
   children: React.ReactNode;
@@ -19,13 +19,13 @@ const AutLayout = async ({ children }: AutLayoutProps) => {
 
         <div className="w-full bg-foreground rounded-2xl p-10 border-t border-t-border-light flex flex-col gap-10">
           {children}
-          <div className="relative w-full">
+          {/* <div className="relative w-full">
             <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-sm bg-foreground px-2">
               OR
             </span>
             <div className="w-full border border-border" />
           </div>
-          <GithubSignInBtn />
+          <GithubSignInBtn /> */}
         </div>
 
         <div className="absolute top-full right-0 translate-x-1/2 -translate-y-1/2 bg-primary w-[100px] h-[100px] rounded-full blur-[180px] -z-10"></div>
